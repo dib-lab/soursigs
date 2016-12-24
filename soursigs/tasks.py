38ca47589a7e283cfaf8d61e4e358c5f28d7aa24
Luiz Irber <irberlui@msu.edu>
2016-12-23 20:27:52 -0500

    Use explicit -T flag for tmpdir on trim-low-abund

--- soursigs/tasks.py
+++ soursigs/tasks.py
@@ -1,5 +1,6 @@
+import os
 from subprocess import CalledProcessError
-from tempfile import NamedTemporaryFile
+from tempfile import NamedTemporaryFile, TemporaryDirectory
 
 from . import app
 
@@ -7,20 +8,23 @@ from . import app
 @app.task
 def compute(sra_id):
     from snakemake import shell
-    with NamedTemporaryFile('w+t') as f:
-        try:
-            shell('fastq-dump -A {sra_id} -Z | '
-                  'head -4000000 | '
-                  'trim-low-abund.py -M 5e8 -k 21 -V -Z 10 -C 3 - -o - | '
-                  'sourmash compute -f -k 21 --dna - -o {output} --name {sra_id}'
-                  .format(sra_id=sra_id, output=f.name))
-        except CalledProcessError as e:
-            # We ignore SIGPIPE, since it is informational (and makes sense,
-            # it happens because `head` is closed and `fastq-dump` can't pipe
-            # its output anymore. More details:
-            # http://www.pixelbeat.org/programming/sigpipe_handling.html
-            if e.returncode != 141:
-                raise e
+    with TemporaryDirectory() as tmpdir:
+        with NamedTemporaryFile('w+t') as f:
+            try:
+                shell('fastq-dump -A {sra_id} -Z | '
+                    'head -4000000 | '
+                    'trim-low-abund.py -T {tmpdir} -M 5e8 -k 21 -V -Z 10 -C 3 - -o - | '
+                    'sourmash compute -f -k 21 --dna - -o {output} --name {sra_id}'
+                    .format(sra_id=sra_id,
+                            output=f.name,
+                            tmpdir=tmpdir))
+            except CalledProcessError as e:
+                # We ignore SIGPIPE, since it is informational (and makes sense,
+                # it happens because `head` is closed and `fastq-dump` can't pipe
+                # its output anymore. More details:
+                # http://www.pixelbeat.org/programming/sigpipe_handling.html
+                if e.returncode != 141:
+                    raise e
 
-        f.seek(0)
-        return f.read()
+            f.seek(0)
+            return f.read()
